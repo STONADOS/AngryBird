6b9d0b559aa7dcdf29473a4583d5b4a33396725a
STONADOS <yogmahajan14345@gmail.com>
2020-08-21 20:41:54 +0530

    addedconstraint

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var log6;
+var slingshot
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,8 +38,11 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    log6 = new Log(230, 180, 80, PI/2);
+
     bird = new Bird(100,100);
-    
+
+    slingshot = new SlingShot(bird.body, log6.body);
 }
 
 function draw(){
@@ -60,6 +66,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
+
+    slingshot.display();
 }
